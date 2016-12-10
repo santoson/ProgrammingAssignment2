99e448181d2f747c112398ce8fb1b324d4f07277
Santoso Nugroho <santoso.n@metamorf.co.id>
2016-12-11 00:44:57 +0700

    Refine comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,6 @@
-## Put comments here that give an overall description of what your
-## functions do
 ## R Programming Assignment 2
 ## Creates R functions that is able to cache the value of a matrix inverse
 
-## Write a short comment describing this function
 ## Creates a special matrix which contains functions to
 ## 1. Set the matrix value
 ## 2. Get the matrix value
@@ -24,7 +21,6 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
 ## cacheSolve returns the inverse of x using the cached value 
 ## in case cached value is null, find the inverse and cached the result
 ## and returns the inverse result
